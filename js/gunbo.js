6b5ef1ae60847c1e89b1d822657d801a73205c9a
MeLling Yang <1216656378@qq.com>
2017-08-11 16:32:33 +0800

    a

--- js/gunbo.js
+++ js/gunbo.js
@@ -1,26 +1,34 @@
-$(function(){
-    var swiper = new Swiper('.swiper-container', {
+$(function() {
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerGroup: 1,
+		slidesPerView: 4,
+		paginationClickable: true,
+		spaceBetween: 0,
+		autoplay: 3000,
+		autoplayDisableOnInteraction: false,
+		spend: 3000,
+		loop: true,
+		prevButton: '.swiper-button-prev',
+		nextButton: '.swiper-button-next',
+		breakpoints: {
+			768: {
+				slidesPerView: 2
+			},
+			480:{
+				slidesPerView: 1
+			}
+			
+		}
+	});
+	$('.jquery-reslider').reSlider({
+		speed: 800, //设置轮播的高度
+		delay: 5000, //设置轮播的延迟时间
+		imgCount: 3, //设置轮播的图片数
+		dots: true, //设置轮播的序号点
+		autoPlay: true //设置轮播是否自动播放
 
-        slidesPerView:4,
-        paginationClickable: true,
-        spaceBetween: 0,
-        autoplay:3000,
-        autoplayDisableOnInteraction:false,
-        spend:3000,
-        loop:true,
-        prevButton:'.swiper-button-prev',
-        nextButton:'.swiper-button-next'
-    });
-    $(function(){
-        $('.jquery-reslider').reSlider({
-            speed:800,//设置轮播的高度
-            delay:5000,//设置轮播的延迟时间
-            imgCount:3,//设置轮播的图片数
-            dots:true,//设置轮播的序号点
-            autoPlay:true//设置轮播是否自动播放
-        });
-    });
-})
-$(function(){
+	});
+});
+(function() {
 	new WOW().init();
 });
