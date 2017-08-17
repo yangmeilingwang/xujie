163fcd53fe40a4bca92722645106c39bd5edde48
MeLling Yang <1216656378@qq.com>
2017-08-17 14:05:01 +0800

    a

--- js/rightside.js
+++
@@ -1,8 +0,0 @@
-//$("#sidecall").click(function(){
-//	$("#sidecall p").toggle();
-//})
-$("#returnTop").click(function () {
-        var speed=500;
-        $('body,html').animate({ scrollTop: 0 }, speed);
-        return false;
- });
